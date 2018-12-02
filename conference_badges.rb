b07cc570bcd0b613d908c0d5d889be782ce39932
Daniel Toma <danieltoma@ubalumni.com>
2018-12-02 07:15:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_arr)
+  new_arr = []
+  name_arr.each { |item| new_arr << badge_maker(item) }
+  new_arr
+end
+
+def assign_rooms(speaker_arr)
+  room_assignments = []
+  speaker_arr.each_with_index do |name, idx|
+    room_assignments[idx] = "Hello, #{name}! You'll be assigned to room #{idx + 1}!"
+  end
+  room_assignments
+end
+
+def printer(arr)
+   
+end
