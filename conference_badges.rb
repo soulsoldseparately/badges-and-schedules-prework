068ff7465b5601aac9c70de8680b4c71afe25899
Daniel Toma <danieltoma@ubalumni.com>
2018-12-02 05:20:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_arr)
+  new_arr = []
+  name_arr.each(|new_arr| << badge_maker()
+end
+
+def assign_rooms(speaker_arr)
+  
+end
+
+def printer
+end
